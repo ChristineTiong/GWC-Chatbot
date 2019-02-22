884ee979a0dba174f216f7525bfea53962e23bf1
ChristineTiong <47904154+ChristineTiong@users.noreply.github.com>
2019-02-22 17:57:07 -0500

    Chat bot
    Girls who code python chat bot.

---
+++ main.py
@@ -0,0 +1,15 @@
+print("Good morning!Or afternoon, which ever one floats your boat")
+answer = input ("Aren't you going to say good morning back???") 
+answer = input ("You know what it's not like I really care anyways hmph.")
+answer = input ("Anyways, now that we conversed a little bit. Pick a number from 1-10 it will be fun.")
+answer = input("Im going to guess you picked the number....hmmmm (press enter to continue)")
+answer = input("7. Was i right? Yes or no")
+print("oah, " + answer + " really?")
+
+for awong in range(0,6):
+  print("hello")
+  
+answer = input("what is your favorite color?")
+while answer != "brown":
+  print("Thats not even a bad color")
+  answer = input("okay then whats your SECOND favorite color?")
